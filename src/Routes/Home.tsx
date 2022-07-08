d10e6964d79111c058dac4cee5e685c3c9352a0b
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-08 23:49:14 +0900

    8.7 Slider part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../untils";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -18,12 +20,12 @@ const Banner = styled.div<{ bgPhoto: string }>`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
   justify-content: center;
+  align-items: flex-start;
   padding: 60px;
-  background-size: cover;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.2)),
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
     url(${props => props.bgPhoto});
+  background-size: cover;
 `;
 
 const Title = styled.h2`
@@ -38,9 +40,48 @@ const Overview = styled.p`
   color: ${props => props.theme.white.lighter};
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -200px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  }
+};
+
 function Home() {
   const { isLoading: isMovieLoading, data: movieData } =
     useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
+
+  const [index, setIndex] = useState(0);
+
+  const incraseIndex = () =>
+    setIndex(prev => {
+      return prev + 1;
+    });
 
   return (
     <Wrapper>
@@ -49,11 +90,31 @@ function Home() {
       ) : (
         <>
           <Banner
+            onClick={incraseIndex}
             bgPhoto={makeImagePath(movieData?.results[0].backdrop_path) || ""}
           >
             <Title>{movieData?.results[0].title}</Title>
             <Overview>{movieData?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{
+                  type: "tween",
+                  duration: .5,
+                }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((item) => (
+                  <Box key={item}>{item}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
